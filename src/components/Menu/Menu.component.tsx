c53d1e8c78fe53af3c317b804d192d0a0bff66e4
Abdallahman Habyarimana <habdallah005@gmail.com>
2025-12-12 05:46:27 -0500

    adding CKEditor to format the text

--- src/components/Menu/Menu.component.tsx
+++ src/components/Menu/Menu.component.tsx
@@ -1,6 +1,6 @@
 
-import { Box, Button, Flex, Heading, HStack, Input, Portal, Menu } from '@chakra-ui/react'
-import { MdOutlineSearch, MdOutlineSettings } from 'react-icons/md'
+import { Box, Button, Flex, Heading, HStack, InputGroup, Input, Portal, Menu } from '@chakra-ui/react'
+import { BsSearch } from 'react-icons/bs'
 import Settings from '../settings/Setting.component'
 
 const MenuSetting = () => {
@@ -8,9 +8,10 @@ const MenuSetting = () => {
     <Flex gap="4" justify="space-between" >
         <Heading marginEnd="auto" >All Task</Heading>
         <HStack width="80" gap={2}>
+           <InputGroup startElement={<BsSearch />}>
             <Input placeholder="search by title, category" />
+           </InputGroup>
             <Settings />
-            
         </HStack>
     </Flex>
   )
